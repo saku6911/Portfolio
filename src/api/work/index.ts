bd11e50a4f20b3d1f294a9c07fbe127dc880fa90
KNM-Shiratori-yk <shiratori_y@kam-i.com>
2024-07-16 16:53:56 +0900

    add

--- src/api/work/index.ts
+++ src/api/work/index.ts
@@ -1,9 +1,5 @@
-import { allList } from "./allList";
-import { content } from "./content";
-import { list } from "./list";
-import { page } from "./page";
 import { topic } from "./topic";
 
 export type { WorkContent } from "./types";
 
-export { list, topic, page, content, allList };
+export {topic};
