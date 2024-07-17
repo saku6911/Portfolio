56f9fc6c65d0e742e1573f2b170126a4f51b8616
KNM-Shiratori-yk <shiratori_y@kam-i.com>
2024-07-17 10:26:37 +0900

    add

--- next.config.js
+++ next.config.js
@@ -20,8 +20,8 @@ module.exports = withBundleAnalyzer({
 		],
 	},
 	env: {
-		API_KEY: process.env.API_KEY,
-		API_DOMAIN: process.env.API_DOMAIN,
+		API_KEY: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
+		API_DOMAIN: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN,
 	},
 	sassOptions: {
 		includePaths: [path.join(__dirname, "src/styles")],
